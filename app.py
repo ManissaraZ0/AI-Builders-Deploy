a29de3609d7ae88f80de6658eb3aa5ae07b4a405
Manissara Saejun <manissara2548@gmail.com>
2022-06-21 13:14:52 +0700

    edit play audio

--- app.py
+++ app.py
@@ -178,9 +178,9 @@ if submit:
     st.success('Congratulation!')
     st.markdown("---")
     # play music
+    st.info(out_pm.fluidsynth())
     with st.spinner(f"Transcribing to FluidSynth"):
-        midi_data = pretty_midi.PrettyMIDI(out_pm)
-        audio_data = midi_data.fluidsynth()
+        audio_data = out_pm.fluidsynth()
         audio_data = np.int16(
             audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
         )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
